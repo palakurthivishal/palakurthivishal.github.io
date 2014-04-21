1d737cef8c3aa4f8f6f35dfde58362e6ab4d60f5
palakurthivishal <palakurthivishal@gmail.com>
2014-04-21 23:57:17 +0530

    Chess pieces

--- app/view/Chessboard.js
+++ app/view/Chessboard.js
@@ -28,7 +28,7 @@ Ext.define('ExtSample.view.Chessboard', {
 						x: a,
 						y: b,
 						bodyStyle: {
-							'background-color': '#fff'
+							'background-color': '#F6CF8A'
 						}
 					});
 				} else {
@@ -38,7 +38,7 @@ Ext.define('ExtSample.view.Chessboard', {
 						x: a,
 						y: b,
 						bodyStyle: {
-							'background-color': '#000'
+							'background-color': '#554536'
 						}
 					});
 				}
@@ -50,7 +50,7 @@ Ext.define('ExtSample.view.Chessboard', {
 						x: a,
 						y: b,
 						bodyStyle: {
-							'background-color': '#000'
+							'background-color': '#554536'
 						}
 					});
 				} else {
@@ -60,13 +60,112 @@ Ext.define('ExtSample.view.Chessboard', {
 						x: a,
 						y: b,
 						bodyStyle: {
-							'background-color': '#fff'
+							'background-color': '#F6CF8A'
 						}
 					});
 				}
 			}
 			pieces.push(piece);
 
+		};
+
+		var whiteKing = '&#9812';
+		var whiteQueen = '&#9813';
+		var whiteRook = '&#9814';
+		var whiteBishop = '&#9815';
+		var whiteKnight = '&#9816';
+		var whitePawn = '&#9817';
+		var blackKing = '&#9818';
+		var blackQueen = '&#9819';
+		var blackRook = '&#9820';
+		var blackBishop = '&#9821';
+		var blackKnight = '&#9822';
+		var blackPawn = '&#9823';
+		var soldier;
+		for (var i = 0; i < pieces.length; i++) {
+			var piece = pieces[i];
+			if (i < 8) {
+				if (i == 7 || i == 0) {
+					soldier = Ext.create('Ext.Img', {
+						src: './images/whiteRook.png',
+						height: 70,
+						width: 70
+					});
+
+				} else if (i == 6 || i == 1) {
+					soldier = Ext.create('Ext.Img', {
+						src: './images/whiteBishop.png',
+						height: 70,
+						width: 70
+					});
+				} else if (i == 5 || i == 2) {
+					soldier = Ext.create('Ext.Img', {
+						src: './images/whiteKnight.png',
+						height: 70,
+						width: 70
+					});
+				} else if (i == 4) {
+					soldier = Ext.create('Ext.Img', {
+						src: './images/whiteQueen.png',
+						height: 70,
+						width: 70
+					});
+				} else {
+					soldier = Ext.create('Ext.Img', {
+						src: './images/whiteKing.png',
+						height: 70,
+						width: 70
+					});
+				}
+			} else if (i >= 8 && i < 16) {
+				soldier = Ext.create('Ext.Img', {
+					src: './images/whitePawn.png',
+					height: 70,
+					width: 70
+				});
+			} else if (i >= 48 && i < 56) {
+				soldier = Ext.create('Ext.Img', {
+					src: './images/blackPawn.png',
+					height: 70,
+					width: 70
+				});
+			} else if (i >= 56 && i < 64) {
+				if (i == 63 || i == 56) {
+					soldier = Ext.create('Ext.Img', {
+						src: './images/blackRook.png',
+						height: 70,
+						width: 70
+					});
+
+				} else if (i == 62 || i == 57) {
+					soldier = Ext.create('Ext.Img', {
+						src: './images/blackBishop.png',
+						height: 70,
+						width: 70
+					});
+				} else if (i == 61 || i == 58) {
+					soldier =Ext.create('Ext.Img', {
+						src: './images/blackKnight.png',
+						height : 70,
+						width : 70
+					});
+				} else if (i == 59) {
+					soldier = Ext.create('Ext.Img', {
+						src: './images/blackQueen.png',
+						height : 70,
+						width : 70
+					});
+				} else {
+					soldier = Ext.create('Ext.Img', {
+						src: './images/blackKing.png',
+						height : 70,
+						width : 70
+					});
+				}
+			}
+			if (soldier)
+				piece.add(soldier);
+			soldier = null;
 		};
 		this.items = pieces;
 		this.callParent();
